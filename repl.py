e2c2f5255548e9e78c097d87312f526f706ee820
Pogobanane <aenderboy@gmx.de>
2026-03-20 10:00:34 +0100

    repl: plot out6-output1

--- repl.py
+++ repl.py
@@ -15,6 +15,7 @@ def foobar():
 
 SIZE_SMALL = 64
 SIZE_BIG = 1522
+DATA = "./data/out6-output1/"
 
 def ns2s(ns) -> float:
     return ns / 1e9
@@ -93,6 +94,14 @@ def plot_example():
     # fig.show()
     display(fig, block=False)
 
+
+
+
+# ========= Data Loaders ===========
+
+
+
+
 def parse_data(systems=None, **kwargs):
 
     if systems is None:
@@ -113,6 +122,45 @@ def parse_data(systems=None, **kwargs):
     df = pd.concat(dfs)
     return df
 
+def parse_latency_csv(systems=None, **kwargs):
+
+    if systems is None:
+        systems = kwargs
+    else:
+        systems = {**systems, **kwargs}
+
+    dfs = []
+    for name, pattern in systems.items():
+        paths = glob.glob(pattern)
+        if not paths:
+            print(f"Warning: no files matched for {name}: {pattern}")
+            continue
+        for p in paths:
+            df = pd.read_csv(p)
+            # override lat_us in df with latency mean from .csv file
+            csv_path = os.path.splitext(p)[0] + ".csv"
+            if os.path.exists(csv_path):
+                lat_df = pd.read_csv(csv_path)
+                # Pktgen produces occasionally wrong latency numbers for minimum sized packets when the timestamp doesnt fit into the packet.
+                # These outliers are typically a latency of 6s for a single sample only, which is clearly wrong as can be seen from the stable throughput numbers.
+                # Mean doesnt work therefore, use median.
+                df["lat_us"] = lat_df["Latency"].quantile(0.5)
+
+            else:
+                print(f"Warning: no matching csv for {p}")
+                df["lat_us"] = np.nan
+            df["system"] = name
+            dfs.append(df)
+
+    df = pd.concat(dfs)
+    return df
+
+
+
+# ========= Base Plots ===========
+
+
+
 def throughput_vs_workload(df, title):
     fig = go.Figure()
     for name in df["system"].unique():
@@ -122,6 +170,42 @@ def throughput_vs_workload(df, title):
                                  mode='lines+markers', name=name))
     fig.update_layout(title=title, xaxis_title='Workload [ns]', yaxis_title='Throughput [Mpps]', yaxis_rangemode='tozero')
     display(fig)
+
+def throughput_vs_memory_workload(df, title):
+    df["memory_workload"] = df["memory_workload"] / 1024
+    fig = go.Figure()
+    for name in df["system"].unique():
+        sdf = df[df["system"] == name].groupby("memory_workload")["Mpps"].agg(["mean", "std"]).reset_index()
+        fig.add_trace(go.Scatter(x=sdf["memory_workload"], y=sdf["mean"],
+                                 error_y=dict(type='data', array=sdf["std"]),
+                                 mode='lines+markers', name=name))
+    fig.update_layout(title=title, xaxis_title='Workload [kB]', yaxis_title='Throughput [Mpps]', yaxis_rangemode='tozero')
+    display(fig)
+
+def throughput_vs_chaining(df, title):
+    fig = go.Figure()
+    for name in df["system"].unique():
+        sdf = df[df["system"] == name].groupby("chaining")["Mpps"].agg(["mean", "std"]).reset_index()
+        fig.add_trace(go.Scatter(x=sdf["chaining"], y=sdf["mean"],
+                                 error_y=dict(type='data', array=sdf["std"]),
+                                 mode='lines+markers', name=name))
+    fig.update_layout(title=title, xaxis_title='Chain length [VNFlets]', yaxis_title='Throughput [Mpps]', yaxis_rangemode='tozero')
+    display(fig)
+
+def latency_vs_workload(df, title):
+    fig = go.Figure()
+    for name in df["system"].unique():
+        sdf = df[df["system"] == name].groupby("workload")["lat_us"].agg(["mean", "std"]).reset_index()
+        fig.add_trace(go.Scatter(x=sdf["workload"], y=sdf["mean"],
+                                 error_y=dict(type='data', array=sdf["std"]),
+                                 mode='lines+markers', name=name))
+    fig.update_layout(title=title, xaxis_title='Workload [ns]', yaxis_title='Latency [us]', yaxis_rangemode='tozero')
+    display(fig)
+
+
+
+# ========= Data Transformers ===========
+
 
 def cpu_normalization(df):
     df["chaining"] = 2
@@ -139,11 +223,15 @@ def cpu_normalization(df):
     return df
 
 
-def foo3(pktsize=64, normalized=False):
+
+# ========= Concrete Plots ===========
+
+
+def throughput(pktsize=64, normalized=False):
     df = parse_data({
-        "Optimal": f"./data/out4/userspace_mirror_b32_*ns_c1_{pktsize}b_rep*.log",
-        "Naive": f"./data/out4/userspace_noiomgr_b32_*ns_c1_{pktsize}b_rep*.log",
-        "Slick": f"./data/out4/userspace_iomgr_b32_*ns_c1_{pktsize}b_rep*.log",
+        "Optimal": f"{DATA}/userspace_mirror_b32_*ns_0b_c2_{pktsize}b_rep*.log",
+        "Naive": f"{DATA}/userspace_noiomgr_b32_*ns_0b_c2_{pktsize}b_rep*.log",
+        "Slick": f"{DATA}/userspace_iomgr_b32_*ns_0b_c2_{pktsize}b_rep*.log",
     })
     title = f"b=32; c=2; pktsize={pktsize}"
     if normalized:
@@ -151,32 +239,68 @@ def foo3(pktsize=64, normalized=False):
         title += " (CPU-normalized)"
     throughput_vs_workload(df, title=title)
 
-def barplot(df, x_axis, hue, title=None):
+def throughput_memorywl(pktsize=64, normalized=False):
+    df = parse_data({
+        "Optimal": f"{DATA}/userspace_mirror_b32_0ns_*b_c2_{pktsize}b_rep*.log",
+        "Naive": f"{DATA}/userspace_noiomgr_b32_0ns_*b_c2_{pktsize}b_rep*.log",
+        "Slick": f"{DATA}/userspace_iomgr_b32_0ns_*b_c2_{pktsize}b_rep*.log",
+    })
+    title = f"b=32; c=2; pktsize={pktsize}"
+    if normalized:
+        df = cpu_normalization(df)
+        title += " (CPU-normalized)"
+    throughput_vs_memory_workload(df, title=title)
+
+def latency(pktsize=64):
+    # df = parse_data({
+    df = parse_latency_csv({
+        "Optimal": f"{DATA}/vm_lat_mirror_b32_*ns_c2_{pktsize}b_rep*.log",
+        "Naive": f"{DATA}/vm_lat_noiomgr_b32_*ns_c2_{pktsize}b_rep*.log",
+        "Slick": f"{DATA}/vm_lat_iomgr_b32_*ns_c2_{pktsize}b_rep1.log", # TODO: rep1 looks excessive, one point in rep0 as well
+    })
+    title = f"b=32; c=2; pktsize={pktsize}"
+    latency_vs_workload(df, title=title)
+
+def chaining(pktsize=64, normalized=False):
+    df = parse_data({
+        "Optimal": f"{DATA}/userspace_mirror_b32_0ns_0b_c*_{pktsize}b_rep*.log",
+        "Naive": f"{DATA}/userspace_noiomgr_b32_0ns_0b_c*_{pktsize}b_rep*.log",
+        "Slick": f"{DATA}/userspace_iomgr_b32_0ns_0b_c*_{pktsize}b_rep*.log",
+    })
+    title = f"b=32; pktsize={pktsize}"
+    if normalized:
+        df = cpu_normalization(df)
+        title += " (CPU-normalized)"
+    throughput_vs_chaining(df, title=title)
+
+def barplot(df, x_axis, x_axis_title, hue, title=None):
     agg = df.groupby([x_axis, hue])["Mpps"].agg(["mean", "std"]).reset_index()
     agg.columns = [x_axis, hue, "Mpps", "std"]
     fig = px.bar(agg, x=x_axis, y="Mpps", error_y="std", color=hue, barmode="group")
-    fig.update_layout(title=title, xaxis_title='Packet size [B]', yaxis_title='Throughput [Mpps]')
+    fig.update_layout(title=title, xaxis_title=x_axis_title, yaxis_title='Throughput [Mpps]')
     display(fig)
 
 def systems():
     df = parse_data({
-        "Optimal": "./data/out3/userspace_mirror_b32_0ns_c1_*.log",
-        "Naive": "./data/out3/userspace_noiomgr_b32_0ns_c1_*.log",
-        "Slick": "./data/out3/userspace_iomgr_b32_0ns_c1_*.log",
+        "Optimal": f"{DATA}/userspace_mirror_b32_0ns_0b_c2_*.log",
+        "Naive": f"{DATA}/userspace_noiomgr_b32_0ns_0b_c2_*.log",
+        "Slick": f"{DATA}/userspace_iomgr_b32_0ns_0b_c2_*.log",
     })
     x_axis = "pktsize"
+    x_title = 'Packet size [B]'
     hue = "system"
-    barplot(df, x_axis, hue, title="b=32; c=2; workload=0ns;")
+    barplot(df, x_axis, x_title, hue, title="b=32; c=2; workload=0ns;")
 
 def batchsizes():
     df = parse_data({
-        "Optimal": "./data/out3/userspace_mirror_b*_0ns_c1_64b_*.log",
-        "Naive": "./data/out3/userspace_noiomgr_b*_0ns_c1_64b_*.log",
-        "Slick": "./data/out3/userspace_iomgr_b*_0ns_c1_64b_*.log",
+        "Optimal": f"{DATA}/userspace_mirror_b*_0ns_0b_c2_64b_*.log",
+        "Naive": f"{DATA}/userspace_noiomgr_b*_0ns_0b_c2_64b_*.log",
+        "Slick": f"{DATA}/userspace_iomgr_b*_0ns_0b_c2_64b_*.log",
     })
     x_axis = "batchsize"
+    x_title = 'Batch size [pkts]'
     hue = "system"
-    barplot(df, x_axis, hue, title="c=2; size=64b; workload=0ns;")
+    barplot(df, x_axis, x_title, hue, title="c=2; size=64b; workload=0ns;")
 
 
 print("Repl.py loaded 🦘")
